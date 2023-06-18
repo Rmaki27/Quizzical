070654177e50a68a2e839df33583f747e8481922
Rmaki27 <engineermaki27@gmail.com>
2023-06-18 10:08:50 -0700

    Get quiz questions data from API and log

--- Quizzical/src/App.tsx
+++ Quizzical/src/App.tsx
@@ -1,19 +1,27 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 import FrontPage from './FrontPage'
 import Quiz from './Quiz'
 
 function App() {
 const [frontPage, setFrontPage] = useState(true)
+const [allQuizData, setAllQuizData] = useState({})
 
-function quizContent(): boolean | undefined {
+function quizContent(): void | undefined {
   if (frontPage !== true) {
     return
   }
   else {
-    setFrontPage(false)
+    return setFrontPage(false)
   }
 }
+
+useEffect(() => {
+  fetch('https://opentdb.com/api.php?amount=5')
+  .then(res => res.json())
+  .then(data => console.log(data))
+}, [])
 
 
   return (
